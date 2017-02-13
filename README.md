6873403a0aba383222faa0fcd043d502fe5fca28
Bernhard Bermeitinger <bernhard.bermeitinger@posteo.de>
2017-02-13 13:50:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# WikipediaAPI
+
+## Building
+
+Requirements:
+* Java 8
+* Maven
+
+Build with 
+
+    mvn package
+    
+Disable Unit tests with
+    
+    mvn package -DskipTests
+
+## Usage
+Install first with (currently not in any maven repo) 
+
+    mvn install
+    
+Then, in your code initialize a new `Wikipedia` instance
+and get a new article:
+
+    Wikipedia w = new Wikipedia();
+    WikipediaArticle article = w.fetchArticle("Passau");
+
+You then have a few possibilities to interact with the article object.
+    
+    // get article's text
+    String text = article.getText()
+    
+    // get article's title
+    String title = article.getTitle()
+  
+    // get article's fetch-date
+    Date date = article.getDate()
+    
+Small configuration can be done in `reference.conf`
---
+++ src/main/java/org/lambda3/data/wikipedia/model/WikipediaArticle.java
@@ -0,0 +1,85 @@
+/*
+ * ==========================License-Start=============================
+ * wiki-api : WikipediaArticle
+ * --------------------------------------------------------------------
+ * Copyright © 2017 Lambda³
+ * --------------------------------------------------------------------
+ * MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * ==========================License-End==============================
+ */
+
+package org.lambda3.data.wikipedia.model;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * Represents an article from Wikipedia.
+ */
+public class WikipediaArticle {
+
+    private final static Gson GSON = new GsonBuilder().create();
+
+    private final String title;
+    private final String text;
+    private final Date date;
+
+    public WikipediaArticle(String title, String text, Date date) {
+        this.title = title;
+        this.text = text;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+
+    @Override
+    public String toString() {
+        return "WikipediaArticle{" +
+                "title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                ", date=" + DateFormat.getDateTimeInstance().format(date) +
+                '}';
+    }
+
+
+    /**
+     * Returns this Wikipedia as a JSON-encoded string.
+     *
+     * @return This article as JSON
+     */
+    public String toJSON() {
+        return GSON.toJson(this);
+    }
+
+}
