7056bdd9448520492d7178d76e4c4a5dc6e5189c
Bernhard Bermeitinger <bernhard.bermeitinger@posteo.de>
2017-06-02 18:32:41 +0200

    - use different config file (wikipedia.local.conf) - use logback.xml to write log files and control behavior - use different java image (8u131-alpine) - update mysql-connector to 6.0.6

--- wikipedia-rest/src/main/resources/start.sh
+++ wikipedia-rest/src/main/resources/start.sh
@@ -10,7 +10,7 @@ cmd="java -jar -server ${JAVA_OPTS} \
 	-Djava.net.preferIPv4Stack=true \
 	-Dfile.encoding=UTF-8 \
 	-Dlogback.configurationFile=${CONFDIR}/logback.xml \
-	-Dconfig.file=${CONFDIR}/wikipedia.conf \
+	-Dconfig.file=${CONFDIR}/wikipedia.local.conf \
 	${LIBDIR}/${project.artifactId}-${project.version}.jar"
 
 exec ${cmd}
